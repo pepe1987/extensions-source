957b18aa58be8fb64395ee06ae7890dc6d2d367b
pepe1987 <killyourpepe@gmail.com>
2024-01-21 09:36:52 -0300

    Update commit-repo.sh

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,15 +2,15 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore --exclude README.md ../main/repo/ .
-git config --global user.email "156378334+keiyoushi-bot@users.noreply.github.com"
-git config --global user.name "keiyoushi-bot"
+git config user.name "$(git log -n 1 --pretty=format:%an)"
+git config user.email "$(git log -n 1 --pretty=format:%ae)"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git commit -m "Update extensions repo"
     git push
 
-    curl https://purge.jsdelivr.net/gh/keiyoushi/extensions@repo/index.min.json
+    // curl https://purge.jsdelivr.net/gh/keiyoushi/extensions@repo/index.min.json
 else
     echo "No changes to commit"
 fi
